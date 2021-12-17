2439046375f64d0af55120d734076b3819b86546
阿伟 <1063870911@qq.com>
2021-12-17 09:59:40 +0800

    delete history

--- .history/src/main_20211215133847.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router/index'
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
-import storeData from "./store/index"
-createApp(App).use(ElementPlus).use(router).use(storeData).mount('#app')
